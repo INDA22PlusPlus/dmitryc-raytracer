ff34c00938350dfc22b6ce8bbdfc900e6f6a40e1
Dmitry Chirin <chirin.dima@gmail.com>
2023-01-07 06:13:11 +0100

    add Vec class

--- main.cpp
+++ main.cpp
@@ -4,6 +4,38 @@
 using namespace std;
 using namespace Magick;
 
+
+class Vec {
+public:
+    double x;
+    double y;
+    double z;
+
+    Vec() {
+        x = y = z = 0;
+    }
+
+    Vec(double x, double y, double z) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+
+//    Vec(int x, int y, int z) {
+//        this->x = x;
+//        this->y = y;
+//        this->z = z;
+//    }
+
+    // Why const point tho?
+    Vec(const double arr[3]) {
+        x = arr[0];
+        y = arr[1];
+        z = arr[2];
+    }
+};
+
+
 int main(int argc,char **argv) {
 //    InitializeMagick(*argv);
 
@@ -18,10 +50,17 @@ int main(int argc,char **argv) {
     img << h << " " << w << endl;
     img << max_color_depth << endl;
 
+    Vec buf[h][w];
+
+//    for (auto row: buf){
+//        for (auto )
+//    }
+
     for (int y = 0; y < h; y++) {
 //        cout << "Line: " << y + 1 << "/" << h << endl;
         for (int x = 0; x < w; x++) {
-            img << x << " " << abs(x - y) << " " << y << endl;
+            Vec color = Vec(x, abs(x - y), y);
+            img << color.x << " " << color.y << " " << color.z << endl;
         }
     }
 
